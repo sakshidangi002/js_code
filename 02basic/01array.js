155fc91e00f8aa425f01f5ee542cb6c3d8c91f32
sakshi dangi <sakshidangi002@gmail.com>
2025-08-21 10:45:25 +0000

    array

---
+++ 02basic/01array.js
@@ -0,0 +1,16 @@
+const my_arr=[0,1,2,3,4]
+console.log(my_arr[1])
+
+//************* array methods**************//
+//my_arr.push(5) 
+//my_arr.push(6)// add the value at the end
+//my_arr.pop() // remove the value at end
+//my_arr.unshift(7) // add value ate start and shift other value
+//my_arr.shift() // remove the unshift value
+console.log(my_arr.includes(9))
+console.log(my_arr.indexOf(9))
+
+const new_arr=my_arr.join() // join array into new varibale and also convert into string
+console.log(my_arr);
+console.log(new_arr);
+
