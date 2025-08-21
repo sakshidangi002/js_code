5cbb4d991353fd36a7c520d86a22dce22a758b4e
sakshi dangi <sakshidangi002@gmail.com>
2025-08-21 10:02:04 +0000

    add math

--- 01basic/02converstion.js
+++ 01basic/02converstion.js
@@ -1,20 +1,29 @@
 // **********************converstion*******************//
 
-let score="28as"
-console.log(typeof(score))
-
-let numvalue=Number(score)
-console.log(typeof(numvalue))
-console.log(numvalue)
-
-let a=2
-console.log(a)
-
-let b=Boolean(a)
-console.log(b)
+//let score="28as"
+//console.log(typeof(score))
+//
+//let numvalue=Number(score)
+//console.log(typeof(numvalue))
+//console.log(numvalue)
+//
+//let a=2
+//console.log(a)
+//
+//let b=Boolean(a)
+//console.log(b)
 
 // **********************operation*******************//
 
-console.log("1"+2) //12
-console.log("1" + 2+2)  // 122
-console.log(2+2+"1") //41
+//console.log("1"+2) //12
+//console.log("1" + 2+2)  // 122
+//console.log(2+2+"1") //41
+
+// **********************string*******************//
+
+const test="sakshi"
+console.log(test.indexOf('a'))
+console.log(test.substring(0,4))
+
+const score=238.9753
+console.log(score.toPrecision(2));
