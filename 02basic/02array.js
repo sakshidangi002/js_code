340ae1c5aaa021a1c58ca9b1ed3036ab3f9fa56f
sakshi dangi <sakshidangi002@gmail.com>
2025-08-21 11:42:13 +0000

    last array

---
+++ 02basic/02array.js
@@ -0,0 +1,20 @@
+const girlA=["sakshi","anchal","priyanka"]
+const girlB=['nitasha','neha','samrity']
+//girlA.push(girlB) // add array into array
+//console.log(girlA)
+
+//const group=girlA.concat(girlB) //merge two array into new array
+//console.log(group);
+
+// spread 
+//const new_group=[...girlA,...girlB]
+//console.log(new_group);
+
+// const arr1=[1,2,3,[4,5],[7,[8,9]]]
+// const arr2=arr1.flat(Infinity) // convert array in array into simple array
+// console.log(arr2);
+ 
+
+ console.log(Array.isArray("sakshi")) // check array or not
+ console.log(Array.from("sakshi")) // convert into arrya
+
