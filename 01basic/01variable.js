3f023ae174e8e9f9b76b2019bccad911792f3514
sakshi dangi <sakshidangi002@gmail.com>
2025-08-21 07:44:48 +0000

    save variable and con

---
+++ 01basic/01variable.js
@@ -0,0 +1,7 @@
+const user_id=101
+let account_email="abc@gmail.com"
+var user_password="1234"
+user_city="shimla"
+let account_state
+
+console.table([user_id,account_email,user_password,user_city])
---
+++ 01basic/02converstion.js
@@ -0,0 +1,20 @@
+// **********************converstion*******************//
+
+let score="28as"
+console.log(typeof(score))
+
+let numvalue=Number(score)
+console.log(typeof(numvalue))
+console.log(numvalue)
+
+let a=2
+console.log(a)
+
+let b=Boolean(a)
+console.log(b)
+
+// **********************operation*******************//
+
+console.log("1"+2) //12
+console.log("1" + 2+2)  // 122
+console.log(2+2+"1") //41
